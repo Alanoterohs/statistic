15c04a93e5835f97dba53d0a7114a3871baf4c92
Alan Otero <65374890+Alanoterohs@users.noreply.github.com>
2023-08-20 20:02:07 -0300

    median update

--- tp1.py
+++ tp1.py
@@ -3,3 +3,11 @@ def average(listNumber: list) -> int:
     for value in listNumber:
         averageReturn += value
     return averageReturn/len(listNumber)
+
+def median(listNumber: list) -> int:
+    sorted(listNumber)
+    index = len(listNumber)//2
+    if(len(listNumber) % 2 == 0):
+        return (listNumber[index-1] + listNumber[index])/2
+    else:
+        return listNumber[index]
